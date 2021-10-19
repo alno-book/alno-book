f8d548610b6b166ab6e8daf51a95ba5eb4a80b99
Federico Izzo <federico.izzo99@gmail.com>
2021-10-19 14:52:57 +0200

    Add color palette for pseudocode

--- src/bin/assets/jquery-pseudocode-init.js
+++ src/bin/assets/jquery-pseudocode-init.js
@@ -1,19 +1,52 @@
-$('.pseudocode').pseudocode({
-    keywords: {
-        'if': '#b8212f',
-        'then': '#b8212f',
-        'else': '#b8212f',
-        'elif': '#b8212f',
-        'for': '#b8212f',
-        'in': '#b8212f',
-        'var': '#b8212f',
-        'function': '#b8212f',
-        'return': '#b8212f',
-        'this': '#b8212f',
-        'max': '#b8212f',
-        'min': '#b8212f',
-        'structure': '#b8212f',
-        'attributes:': '#b8212f',
-        'methods:': '#b8212f',
-    }
-});
+let used_theme_pseudocode = localStorage.getItem("mdbook-theme");
+if (
+    used_theme_pseudocode === 'navy'
+    || used_theme_pseudocode === 'ayu'
+    || used_theme_pseudocode == 'coal') {
+    $('.pseudocode').pseudocode({
+        keywords: {
+            // green
+            'max': '#008200',
+            'min': '#008200',
+            // yellow
+            'for': '#e5c07b',
+            'in': '#e5c07b',
+            'return': '#e5c07b',
+            'if': '#e5c07b',
+            'then': '#e5c07b',
+            'else': '#e5c07b',
+            'elif': '#e5c07b',
+            'var': '#e5c07b',
+            // light blue
+            'function': '#52adf2',
+            // light red
+            'structure': '#e25b51',
+            'attributes:': '#e25b51',
+            'methods:': '#e25b51',
+            'this': '#e25b51',
+        }
+    });
+} else {
+    $('.pseudocode').pseudocode({
+        keywords: {
+            // red
+            'max': '#b8212f',
+            'min': '#b8212f',
+            // purple
+            'for': '#a42fa2',
+            'in': '#a42fa2',
+            'return': '#a42fa2',
+            'if': '#a42fa2',
+            'then': '#a42fa2',
+            'else': '#a42fa2',
+            'elif': '#a42fa2',
+            'var': '#a42fa2',
+            // light blue
+            'function': '#1485ba',
+            'this': '#1485ba',
+            'structure': '#1485ba',
+            'attributes:': '#1485ba',
+            'methods:': '#1485ba',
+        }
+    });
+}
